dc0bbadb985130e2f4cb6c9337ece33cd523c111
Muhammed Abubacker Sufi <sufiabubacker@gmail.com>
2023-11-02 21:33:33 +0530

    footer build

--- portfolio-app/src/Components/Footer/Footer.js
+++ portfolio-app/src/Components/Footer/Footer.js
@@ -7,17 +7,18 @@ import "./Footer.scss"
 export default function Footer() {
   return (
     <section className="footer_section">
-      <div className="footer_container">
-        <div className="footer_socialLinks">
-          <div className="sociallogo">
+      <div className="footer_container container">
+        <div className="footer footer_socialLinks">
+          <div className="footer_socialLogo">
             <h4>find me in:</h4>
-            <TwitterFillIcon className="some-class" size={24}
-             />
-            <FacebookFillIcon className="some-class" size={24}
-              />
+            <TwitterFillIcon className="footer_socialLogo_twitter" size={24}
+            />
+            <FacebookFillIcon className="footer_socialLogo_Facebook" size={24}
+            />
           </div>
-          <div className="gitlogo">
-              <GithubFillIcon  />
+          <div className="footer_githubID" >
+            <h4>@username</h4>
+            <GithubFillIcon className='footer_githubID_logo' size={25} />
           </div>
         </div>
       </div>
--- portfolio-app/src/Components/MainPage/Mainpage.js
+++ portfolio-app/src/Components/MainPage/Mainpage.js
@@ -5,29 +5,32 @@ import Home from "../Pages/Home/Home";
 import { Route, Routes } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
- import './MainPage.scss'
+import './MainPage.scss'
 import Contact from "../Pages/Contact/Contact";
 
 export default function Mainpage(props) {
-     const{MainPageActions,loading}=props
-     useEffect(()=>{
-        setTimeout(()=>{
+    const { MainPageActions, loading } = props
+    useEffect(() => {
+        setTimeout(() => {
             MainPageActions.loading()
-        },2000)
-     },[])
-  return (
-    <React.Fragment>
-        <section className="MainPage_container">
-            {loading?
-        <><Header {...props} /><Routes>
-                      <Route path="/" element={<Home {...props} />} />
-                      <Route path="/_about-me" element={<About {...props} />} />
-                      <Route path="/_projects" element={<Project {...props} />} />
-                      <Route path="/contact" element={<Contact {...props} />} />
-                  </Routes><Footer {...props} /></>
-            :<div className="loading">Loading...</div>}
-        </section>
-    
-    </React.Fragment>
-  );
+        }, 2000)
+    }, [])
+    return (
+        <React.Fragment>
+            <section className="MainPage_container">
+                {loading ?
+                    <>
+                        {/* <Header {...props} /><Routes>
+                            <Route path="/" element={<Home {...props} />} />
+                            <Route path="/_about-me" element={<About {...props} />} />
+                            <Route path="/_projects" element={<Project {...props} />} />
+                            <Route path="/contact" element={<Contact {...props} />} />
+                        </Routes> */}
+                        <Footer {...props} />
+                    </>
+                    : <div className="loading">Loading...</div>}
+            </section>
+
+        </React.Fragment>
+    );
 }
