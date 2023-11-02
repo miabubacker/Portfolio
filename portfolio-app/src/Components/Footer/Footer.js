ef4f028f3e6f8f58bd18b086203c78254e4e6e60
Muhammed Abubacker Sufi <sufiabubacker@gmail.com>
2023-11-02 18:36:56 +0530

    insert icons

--- portfolio-app/src/Components/Footer/Footer.js
+++ portfolio-app/src/Components/Footer/Footer.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import TwitterFillIcon from 'remixicon-react/TwitterFillIcon';
+import FacebookFillIcon from 'remixicon-react/FacebookFillIcon';
+import GithubFillIcon from 'remixicon-react/GithubFillIcon';
+import "./Footer.scss"
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <section className="footer_section">
+      <div className="footer_container">
+        <div className="footer_socialLinks">
+          <div className="sociallogo">
+            <h4>find me in:</h4>
+            <TwitterFillIcon className="some-class" size={24}
+             />
+            <FacebookFillIcon className="some-class" size={24}
+              />
+          </div>
+          <div className="gitlogo">
+              <GithubFillIcon  />
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
