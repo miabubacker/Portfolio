a2cda4f53630b0acc2f54f176ccd47518eeeee2f
Muhammed Abubacker Sufi <sufiabubacker@gmail.com>
2023-11-07 20:58:49 +0530

    projectcard

--- portfolio-app/src/Components/MainPage/MainPage.reducer.js
+++ portfolio-app/src/Components/MainPage/MainPage.reducer.js
@@ -4,43 +4,52 @@ import { produce } from "immer";
 import ReactjsFillIcon from "remixicon-react/ReactjsFillIcon";
 import Html5FillIcon from "remixicon-react/Html5FillIcon";
 import Css3FillIcon from "remixicon-react/Css3FillIcon";
+import VuejsFillIcon from "remixicon-react/VuejsFillIcon";
 // import JavascriptFillIcon from "remixicon-react/java";
+import cardImg from "../Assets/Img/cardImg.svg";
+import gameCard from "../Assets/Img/cardImggame.svg";
+import cardEteram from "../Assets/Img/cardImgEtheram.svg";
+
+
 const initialState = {
   loading: false,
   hamburger: false,
   dashboard: [],
   home: [],
   about: [],
-   typeofProject:[{icon:<ReactjsFillIcon/>,name:'React'},{icon:<Html5FillIcon/>,name:'HTML'},{icon:<Css3FillIcon/>,name:'CSS'}],
+  typeofProject: [{ icon: <ReactjsFillIcon />, name: 'React' }, { icon: <Html5FillIcon />, name: 'HTML' }, { icon: <Css3FillIcon />, name: 'CSS' }],
   projects: [
     {
       project: "react",
       title: "Drop KIck",
+      img :cardImg,
       about_us: "Duis aute irure dolor in velit esse cillum dolore.",
-      projectIcon: <ReactjsFillIcon />,
-       project_link:'https://github.com/miabubacker/kick-app'
+      Project_Icon: <ReactjsFillIcon />,
+      project_link: 'https://github.com/miabubacker/kick-app'
     },
     {
       project: "react",
       title: "tesla ",
+      img: gameCard,
       about_us: "Duis aute irure dolor in velit esse cillum dolore.",
-      projectIcon: <ReactjsFillIcon />,
-       project_link:'https://github.com/miabubacker/kick-app'
+      Project_Icon: <ReactjsFillIcon />,
+      project_link: 'https://github.com/miabubacker/kick-app'
     },
     {
       project: "html",
       title: "layout",
+      img:cardEteram,
       about_us: "Duis aute irure dolor in velit esse cillum dolore.",
-      projectIcon: <Html5FillIcon />,
-       project_link:'https://github.com/miabubacker/kick-app'
+      Project_Icon: <VuejsFillIcon/>,
+      project_link: 'https://github.com/miabubacker/kick-app'
     },
-    {
-      project: "css",
-      title: " resposive",
-      about_us: "Duis aute irure dolor in velit esse cillum dolore.",
-      projectIcon: <Css3FillIcon />,
-      project_link:'https://github.com/miabubacker/kick-app'
-    },
+      // {
+      //   project: "css",
+      //   title: " resposive",
+      //   about_us: "Duis aute irure dolor in velit esse cillum dolore.",
+      //   projectIcon: <Css3FillIcon />,
+      //   project_link: 'https://github.com/miabubacker/kick-app'
+      // },
   ],
   headers: ["_hello", "_about-me", "_projects"],
   filterProjects: [],
@@ -57,7 +66,7 @@ export default handleActions(
       produce(state, (draft) => {
         draft.hamburger = !state.hamburger;
       }),
-      [ACTION_TYPES.PROJECT]: (state, { data } = {}) =>
+    [ACTION_TYPES.PROJECT]: (state, { data } = {}) =>
       produce(state, (draft) => {
         draft.filterProjects = data;
       })
