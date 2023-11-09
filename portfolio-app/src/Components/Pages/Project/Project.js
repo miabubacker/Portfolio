75803693116643492344c63a09e4ffc481d53621
Muhammed Abubacker Sufi <sufiabubacker@gmail.com>
2023-11-09 19:49:11 +0530

    finalCheck

--- portfolio-app/src/Components/Pages/Project/Project.js
+++ portfolio-app/src/Components/Pages/Project/Project.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Checkbox,Collapse } from "antd";
+import { Checkbox, Collapse } from "antd";
 import CloseFillIcon from "remixicon-react/CloseFillIcon"
 import ArrowDownSFillIcon from "remixicon-react/ArrowDownSFillIcon";
 import "./Project.scss";
@@ -10,7 +10,11 @@ export default function Project(props) {
   const { Panel } = Collapse
   const { typeofProject, projects, MainPageActions, filterProjects } = props;
   const [selectproject, setSelectProject] = useState([]);
-  const handleCheckbox = (filterproject) => {
+  const [btnindex, setBtnindex] = useState([])
+
+
+  const handleCheckbox = (filterproject, index) => {
+
     let select = [...selectproject];
     if (select.includes(filterproject)) {
       const toggle = select.filter((data) => data !== filterproject);
@@ -18,6 +22,14 @@ export default function Project(props) {
     } else {
       setSelectProject([...select, filterproject]);
     }
+    const selectedbtn = [...btnindex]
+    if (selectedbtn.includes(index)) {
+      const toggle = selectedbtn.filter((data) => data !== index);
+      setBtnindex(toggle);
+    } else {
+      setBtnindex([...selectedbtn, index]);
+    }
+
   };
 
   useEffect(() => {
@@ -26,68 +38,65 @@ export default function Project(props) {
     );
   }, [selectproject]);
 
-const closeBtn=()=>{
-  setSelectProject([])
-}
+  const closeBtn = () => {
+    setSelectProject([])
+  }
   return (
     <>
-    
       <section className="Project_section">
-
-        
-
+        <p className="toptitle">_Projects</p>
         {/* leftcolumn */}
         <div className="Project_menu">
-        <Collapse
-          bordered={false}
-          className="projectCollapse"
-          defaultActiveKey={["1"]}
-          expandIcon={({ isActive }) => (
-            <div>
-              <ArrowDownSFillIcon
-                className={`${isActive
-                    ? "collapseHeaderIconExpand"
-                    : "collapseHeaderIconCollapse"
-                  }`}
-              />
-            </div>
-          )}
-          expandIconPosition="left"
-          onChange={() => { }}
-        >
-        <Panel key="1" header="Projects">
-            {/* <div> */}
-            {typeofProject.map((domain, index) => (
-            <div key={index} className="left_main">
-              <Checkbox
-                onChange={(e) => {
-                  handleCheckbox(e.target.value);
-                }}
-                checked={selectproject.includes(domain.name.toLowerCase())}
-                value={domain.name.toLowerCase()}
-              >
-              </Checkbox>
-              <div>{domain.icon}</div>
-              <div><p>{domain.name}</p></div>
-            </div>
-          ))}
-            {/* </div> */}
-          </Panel>
+          <Collapse
+            bordered={false}
+            className="projectCollapse"
+            defaultActiveKey={["1"]}
+            expandIcon={({ isActive }) => (
+              <div>
+                <ArrowDownSFillIcon
+                  className={`${isActive
+                    ? "collapseHeaderIconExpands"
+                    : "collapseHeaderIconCollapsed"
+                    }`}
+                />
+              </div>
+            )}
+            expandIconPosition="left"
+            onChange={() => { }}
+          >
+            <Panel key="1" header="Projects">
+              {/* <div> */}
+              {typeofProject.map((domain, index) => (
+                <div key={index} className="left_main">
+                  <Checkbox
+                    onChange={(e) => {
+                      handleCheckbox(e.target.value,index);
+                    }}
+                    checked={selectproject.includes(domain.name.toLowerCase())}
+                    value={domain.name.toLowerCase()}
+                  >
+                  </Checkbox>
+                  <div className={btnindex.includes(index) ? 'isActive' : 'false'}>{domain.icon}</div>
+                  <div className={btnindex.includes(index) ? 'isActive' : 'false'}><p>{domain.name}</p></div>
+                </div>
+              ))}
+              {/* </div> */}
+            </Panel>
           </Collapse>
         </div>
-
         <div className="Projects">
           <div className="Project_title">
             {/*  header */}
-            {selectproject.length>0&&<div>
+            {selectproject.length > 0 && <div>
+              <p>// Projects:</p>
               {selectproject.map((data) => (
                 <span key={data}>{data};</span>
               ))}
               <CloseFillIcon className="closeBtn" size={18}
-                onClick={() => {closeBtn()}}
+                onClick={() => { closeBtn() }}
               />
             </div>
-}
+            }
 
           </div>
           {/* projects */}
