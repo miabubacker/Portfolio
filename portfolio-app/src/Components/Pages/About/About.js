6474bff939456c2384851e04ba7de32cb789add0
unknown <miabubacker98@gmail.com>
2023-11-21 16:02:54 +0530

    remove code comment

--- portfolio-app/src/Components/Pages/About/About.js
+++ portfolio-app/src/Components/Pages/About/About.js
@@ -213,33 +213,6 @@ export default function About(props) {
               ))}
             </div>
             <div className="text">
-              {/* /**
-              <br />
-              * About me
-              <br />
-              * I have 5 years of еxperience in web
-              <br />
-              * development lorem ipsum dolor sit amet, <br />
-              * consectetur adipiscing elit, sed do eiusmod
-              <br />
-              * tempor incididunt ut labore et dolore
-              <br />
-              * magna aliqua. Ut enim ad minim veniam,
-              <br />
-              * quis nostrud exercitation ullamco laboris
-              <br />
-              * nisi ut aliquip ex ea commodo consequat.
-              <br />
-              * Duis aute irure dolor in reprehenderit in
-              <br />
-              *<br />
-              * Duis aute irure dolor in reprehenderit in
-              <br />
-              * voluptate velit esse cillum dolore eu fugiat <br />
-              * nulla pariatur. Excepteur sint occaecat <br />
-              * officia deserunt mollit anim id est laborum.
-              <br />
-              */ }
               <Aboutme  lineCount={lineCount} setLineCount={setLineCount}/>
             </div>
           </div>
